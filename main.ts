1ac6113639dcdf6e60b150ae8e45d395eae6bf59
a8365885 <z8365885@gmail.com>
2019-01-29 17:46:44 +0800

    add block name

--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
-/*
-STLeee
-load dependency
-"robotbit_mbot": "file:../pxt-robotbit_mbot"
-*/
-
-//% color="#7F7FFF" weight=10 icon="\uf7c0"
+//% color="#7F7FFF" weight=10 icon="\uf7c0" block="MBot"
 namespace robotbit_mbot
 {
     //% block
-    export function test(v: number, interval: number = 150): void
-    { }
+    export function test(value: number): void
+    { 
+        
+    }
 }
