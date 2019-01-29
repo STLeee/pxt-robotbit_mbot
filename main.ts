56da39f213db0a0601da76f5d3df992b4dba0f78
a8365885 <z8365885@gmail.com>
2019-01-29 17:19:29 +0800

    block: showNumber

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% color="#7F7FFF" weight=10 icon="\uf7c0"
+namespace robotbit_mbot
+{
+    //% blockId=robotbit_mbot_show_number
+    //% block="show|number %v"
+    //% v.min=0 v.max=9
+    export function showNumber(v: number, interval: number = 150): void
+    { 
+        serial.writeString(v + "\r\n");
+    }
+}
