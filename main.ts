88b4d078affa1d73ce1f34a4fb4626fcd39ddc90
a8365885 <z8365885@gmail.com>
2019-01-29 17:32:42 +0800

    block: test

--- main.ts
+++ main.ts
@@ -7,5 +7,7 @@ load dependency
 //% color="#7F7FFF" weight=10 icon="\uf7c0"
 namespace robotbit_mbot
 {
-
+    //% block
+    export function test(v: number, interval: number = 150): void
+    { }
 }
