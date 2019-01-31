0b3957605a505f3be7ba9dc3e79659c9ea19e170
a8365885 <z8365885@gmail.com>
2019-01-31 17:44:24 +0800

    test ppin to digitalpin

--- main.ts
+++ main.ts
@@ -3,28 +3,35 @@ namespace robotbit_mbot
 {
     export enum PPin {
         //% block="P0"
-        PPin0 = 0,
+        P0 = DigitalPin.P0,
         //% block="P1"
-        PPin1 = 1,
+        P1 = DigitalPin.P1,
         //% block="P2"
-        PPin2 = 2,
+        P2 = DigitalPin.P2,
         //% block="P8"
-        PPin8 = 8,
+        P8 = DigitalPin.P8,
         //% block="P12"
-        PPin12 = 12,
+        P12 = DigitalPin.P12,
         //% block="P13"
-        PPin13 = 13,
+        P13 = DigitalPin.P13,
         //% block="P14"
-        PPin14 = 14,
+        P14 = DigitalPin.P14,
         //% block="P15"
-        PPin15 = 15,
+        P15 = DigitalPin.P15,
     }
     
     //% blockId=me_line_follower_sensor
     //% block="Me Line Follower Sensor|pin1 %ppin1|pin2 %ppin2"
-    export function me_line_follower_sensor(ppin1: PPin, ppin2: PPin): number
+    export function me_line_follower_sensor(ppin1: PPin, ppin2: PPin): number[]
     {
-        return 0
+        var ppin1str: string = PPin[ppin1];
+        var ppin2str: string = PPin[ppin2];
+        var digitalpin1: DigitalPin = DigitalPin[ppin1str]
+        var digitalpin2: DigitalPin = DigitalPin[ppin2str]
+        let res1 = pins.digitalReadPin(digitalpin1)
+        let res2 = pins.digitalReadPin(digitalpin2)
+
+        return [digitalpin1, digitalpin2]
     }
     
     //% blockId=me_ultrasonic_sensor
